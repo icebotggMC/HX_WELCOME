5481dff18315d9c0debd47adf8dcc0628d6eb6de
Dood <41833445+icebotggMC@users.noreply.github.com>
2019-05-15 01:05:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ client.on('message', msg => {
 
 client.on('ready', () => {
     console.log('Bot Is Ready Now !');
-    client.user.setGame(`FFUN | Welcome`,"http://twitch.tv/xkilleryt")
+    client.user.setGame(`HX| Welcome`,"http://twitch.tv/xkilleryt")
     });
 
 
